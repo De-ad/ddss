100bda83b6f6ca0ead72560e9eeb4915f0633c52
Dead <space.gee.z@gmail.com>
2024-03-18 13:55:23 +0300

    Add model script

--- lab_1/scp.sh
+++ lab_1/scp.sh
@@ -1,2 +1,3 @@
 scp -P 2222 script.sql s334512@helios.se.ifmo.ru:/home/studs/s334512/
 scp -P 2222 run_script.sh s334512@helios.se.ifmo.ru:/home/studs/s334512/
+scp -P 2222 model.sql s334512@helios.se.ifmo.ru:/home/studs/s334512/
