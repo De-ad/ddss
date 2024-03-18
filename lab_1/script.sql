1821782d817314bb5b79fef03a125d23fdf82b8d
Dead <space.gee.z@gmail.com>
2024-03-18 14:28:57 +0300

    Fix script and result

--- lab_1/script.sql
+++ lab_1/script.sql
@@ -11,20 +11,20 @@ DECLARE
     prev_attr TEXT := '';
     pointer CURSOR FOR (
         SELECT
-            patt.attnum num, 
-            patt.attname master_att,
-            pt.typname, 
-            pcstr.contype, 
-            pcstr.confrelid,
-            patt2.attname slave_att, 
-            pclass2.relname 
-        FROM pg_class pclass 
-        JOIN pg_attribute patt ON pclass.oid = patt.attrelid 
-        JOIN pg_type pt ON pt.oid = patt.atttypid 
-        JOIN pg_namespace pn ON pclass.relnamespace = pn.oid 
-        LEFT JOIN pg_constraint pcstr ON patt.attrelid = pcstr.conrelid AND patt.attnum = ANY(pcstr.conkey)
-        LEFT JOIN pg_attribute patt2 ON pcstr.confrelid = patt2.attrelid AND pcstr.confkey[1] = patt2.attnum 
-        LEFT JOIN pg_class pclass2 ON pclass2.oid = patt2.attrelid 
+            patt.attnum num, -- номер аттрибута
+            patt.attname master_att, -- имя аттрибута
+            pt.typname, -- имя типа данных
+            pcstr.contype, -- 'f', если fk --
+            pcstr.confrelid, -- oid таблицы, на которую ссылается (если contype = 'f')
+            patt2.attname slave_att, -- имя аттрибута
+            pclass2.relname -- имя таблицы
+        FROM pg_class pclass -- все объекты, подобные таблицам
+        JOIN pg_attribute patt ON pclass.oid = patt.attrelid -- +аттрибуты, которые принадлежат таблицам
+        JOIN pg_type pt ON pt.oid = patt.atttypid -- +типы аттрибутов
+        JOIN pg_namespace pn ON pclass.relnamespace = pn.oid -- +пространство имен для таблицеподобной сущности
+        LEFT JOIN pg_constraint pcstr ON patt.attrelid = pcstr.conrelid AND patt.attnum = ANY(pcstr.conkey) -- +ограничения. LEFT чтобы оставить те, что без ограничений. ANY для исключения повторов
+        LEFT JOIN pg_attribute patt2 ON pcstr.confrelid = patt2.attrelid AND pcstr.confkey[1] = patt2.attnum -- +названия аттрибутов, на которые ссылаемся через fk
+        LEFT JOIN pg_class pclass2 ON pclass2.oid = patt2.attrelid -- +имя таблицы, на который ссылается наш fk
         where pclass.relname = t_name and pn.nspname = s_name and patt.attnum > 0
     );
 
@@ -46,7 +46,7 @@ BEGIN
     LOOP
         IF prev_attr <> c.master_att THEN
             IF prev_attr <> '' THEN
-                RAISE NOTICE '%  %  %', no_delimeter, name_delimeter, attr_delimeter;
+                RAISE NOTICE ' ';
             END IF;
             RAISE NOTICE '%  %  Type: %', RPAD(c.num::TEXT, 3, ' '), RPAD(c.master_att, 11, ' '), c.typname;
             IF c.contype = 'f' THEN
